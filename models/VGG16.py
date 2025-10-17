a33af9e63834ad85466181c6def10f8788a3a06f
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-10-17 22:17:39 +0300

    Create VGG16.py

---
+++ models/VGG16.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+
+class VGG16(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(VGG16, self).__init__()
+        self.features = nn.Sequential(
+            # Block 1
+            nn.Conv2d(3, 64, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            # Block 2
+            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            # Block 3
+            nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            # Block 4
+            nn.Conv2d(256, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            # Block 5
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096), nn.ReLU(True), nn.Dropout(),
+            nn.Linear(4096, 4096), nn.ReLU(True), nn.Dropout(),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
